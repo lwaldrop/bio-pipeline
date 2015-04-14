147b22681c889731b937e21e711c5c6979e4f17f
Lindsay Waldrop <lwaldrop@email.unc.edu>
2015-04-14 15:20:44 -0400

    Do you have to put this here?

--- R_pmscript.R
+++ R_pmscript.R
@@ -1,1 +1,14 @@
 # This is a test R script.
+
+setwd("/Users/lwaldrop/Desktop/Bio pipeline Waldrop/stormevents") #Sets working directory
+getwd() #Prints working directory
+filenames<-list.files() #Pulls file names and creates a list of them
+class(filenames) #Returns type of class of filenames
+
+storms<-read.csv(file=filenames[1]) #Reads in the first storm name file
+storms2<-read.csv(file=filenames[2])
+
+testcase<-lm(INJURIES_DIRECT~DEATHS_DIRECT,data=storms)
+summary(testcase)
+anova(testcase)
+plot(INJURIES_DIRECT~DEATHS_DIRECT,data=storms)
